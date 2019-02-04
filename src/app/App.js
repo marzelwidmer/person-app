2c6552ec6d5323ab6634bed9057e473191b067f1
Marcel Widmer <marzelwidmer@gmail.com>
2019-02-04 20:39:11 +0100

    add Docker File

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
  import TopNav from "../layout/TopNav"
 import "./App.css"
 import GitHub from "../github/GitHub";
-import Person from "../person/Person";
 import FetchPerson from "../person/FetchPerson";
 
 class App extends Component {
@@ -12,7 +11,6 @@ class App extends Component {
        <div className="App">
          <TopNav />
          {/* <GitHub username="c3smonkey" className="App-main"> </GitHub> */}
-         {/* <Person></Person> */}
          <FetchPerson />
        </div>
      )
